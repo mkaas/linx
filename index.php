e3d894cacb557667276b14c4f12145ce1b66e673
Mikkel Kaas <mikkel@uniqma.dk>
2012-02-23 09:10:37 +0100

    -

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 	/* LinX 2012 */
 	
 	include_once('includes/functions.inc.php');
-	htmlHeader('LinX','2012','a');
+	htmlHeader('LinX','2012','micromalist');
 	
+	$rows=2000;
+	$cols=40;
+	$space_width=50;
 	
+	print '<table border="1" cellspacing="0" cellpadding="0">';
+	for($i=1;$i<=$rows;$i++){
+		print '<tr>';
+		for($n=1;$n<=$cols;$n++){
+			print '<td style="width:'.$space_width.'px" class="link">'.$i.':'.$n.'</td>';
+		}
+		print '</tr>';
+	}
+	print '</table>';
 	
 	htmlFooter();
 
